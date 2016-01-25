6b64f43494d71db37bd8cf94186655819f172305
Umer Khurramov <umer.hurramov@gmail.com>
2016-01-25 18:28:30 +0300

    add 404 page and delete some garbage

--- app/views/errors/missing.blade.php
+++ app/views/errors/missing.blade.php
@@ -1,3 +1,14 @@
-<?php
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>404 Not Found</title>
+  <link rel="stylesheet" href="/evth/public/css/newmember.css">
+</head>
+<body class="page-404">
+  <div class="page-404__info">
+    <span class="page-404__title">Данная страница не существует</span>
+    <a href="/" class="page-404__button">На главную</a>
+  </div>
+</body>
+</html>
