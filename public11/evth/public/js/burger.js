fd736ff3ad229286e860dcf036679f450fdd4b10
Ivan <Ivan@site.ru>
2016-01-25 17:25:46 +0200

    shljapa

--- public11/evth/public/js/burger.js
+++
@@ -1,9 +0,0 @@
-(function($){
-    $.fn.burger = function(){
-        var sidebar = $($(this).attr('data-sidebar'));
-        $(this).on('click', function(){
-            $(this).toggleClass('active');
-            sidebar.toggleClass('active');
-        });
-    };
-})(jQuery);
--- public11/evth/public/js/member.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function(){
-    $('.burger-button').burger();
-});
