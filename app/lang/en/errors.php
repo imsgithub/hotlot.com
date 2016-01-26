04f368d629aaa1d0f6cd165edf39c92cd9fa84b0
Ivan <Ivan@site.ru>
2016-01-26 10:31:09 +0200

    add 404 lang

---
+++ app/lang/en/errors.php
@@ -0,0 +1,6 @@
+<?php
+return[
+	'title' => '404 Not Found',
+	'page_not_exists' => 'This page does not exist',
+	'to_main' => 'To main',
+];
--- app/views/errors/missing.blade.php
+++ app/views/errors/missing.blade.php
@@ -1,14 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+	{{App::setLocale(Session::get('lang'))}}
   <meta charset="UTF-8">
-  <title>404 Not Found</title>
+  <title>{{Lang::get('errors.title')}}</title>
   <link rel="stylesheet" href="/evth/public/css/newmember.css">
 </head>
 <body class="page-404">
   <div class="page-404__info">
-    <span class="page-404__title">Данная страница не существует</span>
-    <a href="/" class="page-404__button">На главную</a>
+    <span class="page-404__title">{{Lang::get('errors.page_not_exists')}}</span>
+    <a href="/" class="page-404__button">{{Lang::get('errors.to_main')}}</a>
   </div>
 </body>
 </html>
