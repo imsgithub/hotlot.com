20a06af990ee61632086bbade265f1a8d917d9a4
Ivan <Ivan@site.ru>
2016-01-26 11:16:41 +0200

    code remarketing

--- app/views/counters/metrika.blade.php
+++ app/views/counters/metrika.blade.php
@@ -1,5 +1,5 @@
 <!-- Yandex.Metrika counter -->
-<!--script type="text/javascript">
+<script type="text/javascript">
 (function (d, w, c) {
 (w[c] = w[c] || []).push(function() {
 try {
@@ -25,7 +25,7 @@ d.addEventListener("DOMContentLoaded", f, false);
 </script>
 <noscript><div><img src="//mc.yandex.ru/watch/32691885" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
 <!-- /Yandex.Metrika counter -->
-<!--script>
+<script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -35,7 +35,7 @@ ga('send', 'pageview');
 </script>
 <script type="text/javascript">
 /* <![CDATA[ */
-var google_conversion_id = 948721330;
+var google_conversion_id = 934685651;
 var google_custom_params = window.google_tag_params;
 var google_remarketing_only = true;
 /* ]]> */
@@ -48,4 +48,4 @@ var google_remarketing_only = true;
 <div style="display:inline;">
 <img height="1" width="1" style="border-style:none;" alt="" src="//googleads.g.doubleclick.net/pagead/viewthroughconversion/948721330/?value=0&amp;guid=ON&amp;script=0"/>
 </div>
-</noscript-->
+</noscript>
